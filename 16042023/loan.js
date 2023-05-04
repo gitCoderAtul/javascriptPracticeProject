02d73b3a0ab75d305f99f55f83df180f3e616015
unknown <atulk9996@gmail.com>
2023-05-04 07:34:00 +0530

    to to list - add and delete update V1 --DOM Properties, DOM Methods,  DOM event listerner, DOM Selector Method, Foreach Methood, DOM Event Handler, Array Method, DOM event listener

--- 16042023/loan.js
+++ 16042023/loan.js
@@ -18,15 +18,73 @@ function myfunc(){
 //convert string to number using + operator or parseInt....
     var amount = +txt1.value;
     var tenure = +txt2.value;
-    var Interest = +txt3.value;
+    var roi = +txt3.value;
 
     // console.log(amount,tenure,Interest);
-    var p = amount;
-    var R = tenure * 10;
-    var N = Interest;
+    var P = amount;
+    var N = tenure * 12;
+    var R = roi/12/100;
 
-    var EMI = p * R * (1+R)**N / ( (1+R**N-1) );
-    console.log(EMI);
+    var EMI = P * R * (1+R)**N / ( (1+R)**N-1);
+    EMI = Math.round(EMI);
+    // console.log(EMI);
+
+    var total = EMI * N;
+    console.log(total);
+    
+    var InterestValue = total - P;
+    console.log(InterestValue);
+
+    var [span1,span2,span3,span4] = document.getElementsByTagName('span');
+    console.log(span1);
+
+    span1.innerHTML = EMI;
+    span2.innerHTML = P;
+    span3.innerHTML = InterestValue;
+    span4.innerHTML = total;
+
+    // Data retrieved from https://netmarketshare.com
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Loan Calculator',
+        align: 'left'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Loan',
+        colorByPoint: true,
+        data: [{
+            name: 'Loan Amount',
+            y: P,
+        }, {
+            name: 'Interest',
+            y: InterestValue,
+        },]
+    }]
+});
 
 
 }
--- 30042023/to-do-list.js
+++ 30042023/to-do-list.js
@@ -1,3 +1,4 @@
+ //Array Preparartion
  var taskList = [
     "Workout",
     "BathTime",
@@ -21,52 +22,91 @@
  //foreach
 
  console.log(document.querySelector('#all-task'));
-
-
- taskList.forEach((val, index)=>{
-console.log(val);
-var ulTag = document.querySelector('#all-task');
-var liTag = document.createElement('li');
-liTag.className ='nav-item p-3 border-bottom';
-
-var span1 = document.createElement('span');
-span1.innerHTML = val;
-
-var span2 = document.createElement('span');
-span2.innerHTML = 'x';
-span2.className = 'abc';
-// console.log(liTag);
-
-ulTag.append(liTag);
-liTag.append(span1);
-liTag.append(span2);
-  
-span2.addEventListener('click', deleteTask);
-
- });
  
-document.querySelector('#btn-add').onclick = function(){
-
-    console.log('add');
+ function listDownValue(){
     
+    //foreach method
+    taskList.forEach((val, index)=>{
+        console.log(val);
+
+        //DOM Selector Method
+        var ulTag = document.querySelector('#all-task');
+        var liTag = document.createElement('li');
+
+        //DOM Properties
+        liTag.className ='nav-item p-3 border-bottom';
+        
+        var span1 = document.createElement('span');
+        
+        //DOM Properties
+        span1.innerHTML = val;
+        
+        var span2 = document.createElement('span');
+        span2.innerHTML = 'x';
+        span2.className = 'abc';
+        // console.log(liTag); 
+        
+        //DOM Method (object -> object )
+        liTag.append(span1);
+        
+        //DOM event listener
+        span2.addEventListener('click', deleteTask);
+        liTag.append(span2);
+        ulTag.append(liTag); 
+        
+         }); 
+ }
+ listDownValue();
+
+ //DOM Event Handler
+document.querySelector('#btn-add').onclick = function(){
+    console.log('add');
+    console.log(document.getElementById('txt').value);
+    var textBoxValue = document.getElementById('txt');
+
+  //Array Method
+    taskList.unshift(textBoxValue.value);
+    console.log(taskList);
+    document.querySelector('#all-task').innerHTML = '';
+
+    listDownValue();
+    // console.log(textBoxValue);
+    textBoxValue.value = '';
 
 }
 
- function listDownValue(){
-
-
-
- }
+ 
 
  function deleteTask(){
-console.log('delete');
+// console.log('delete');
+
 console.log(this);
-console.log(this.parentElement);
+ //DOM Traverse property
+    // var ans = confirm("Do you wanna Delete?");
+    // if(ans){
+    //     this.parentNode.remove();
+    // }
+
+// console.log(this.parentElement);
+
+//DOM Traverse or Navigation Property
+console.log(this.previousElementSibling);
+ console.log(this.previousElementSibling.innerText);
+// console.log( taskList.indexOf(this.previousElementSibling.innerText));
+
+//Array Method indexOf()
+var indexNo = taskList.indexOf(this.previousElementSibling.innerText);
+console.log(indexNo);
+
+//Array Methods
+taskList.splice(indexNo, 1);
+console.log(taskList);
+
+ //DOM Traverse or Navigation Property -- parentNode
+
+// console.log(this.parentNode);
+this.parentNode.remove();
+ 
  }
-
-
- function addTask() {
-    
-
-
- }
+ 
+  
